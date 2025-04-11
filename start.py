398d313fe25cc204f495080674748c6563184111
Ricky Tsai <ricky@rtnztech.com>
2025-04-12 08:21:08 +1200

    Remove test and rename title

--- start.py
+++ start.py
@@ -1,5 +1,9 @@
-import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from datetime import timedelta
+import os
 
 from fastapi_jwt import JWT
 from auth import auth
@@ -7,14 +11,14 @@ from auth import auth
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import *
 
-app = FastAPI(title='test', docs_url=None)
+app = FastAPI(title='Music', docs_url=None)
 JWT(
     app,
     '07E38D73B7DF0C511753B2E6229BAE701EC6E2273FDEC78F658EDB87C81B83A3'
     '766C8BC0EA276121099BF3F609E26536EFCFD4B37FA261C5ECE1D9F2B7976DC7',
     timedelta(hours=1),
     timedelta(days=1),
-    os.getenv('LOGIN_BYPASS')=='true'
+    os.getenv('LOGIN_BYPASS') == 'true'
 )
 app.add_middleware(
     CORSMiddleware,
@@ -24,11 +28,5 @@ app.add_middleware(
         'http://localhost:8100'
     ]
 )
-
-
-@app.get('/')
-async def base():
-    return 'test create'
-
 
 app.include_router(auth)
