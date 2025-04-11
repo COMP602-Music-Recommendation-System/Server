c9cae0c53d0c4433da736574f28a4152b66f4ba0
Ricky Tsai <ricky@rtnztech.com>
2025-04-12 06:33:11 +1200

    Add database

---
+++ database/__init__.py
@@ -0,0 +1,66 @@
+from uuid import uuid4
+
+from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, DateTime, Boolean
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship, mapped_column
+
+Base = declarative_base()
+
+
+class PlaylistMap(Base):
+    __tablename__ = 'Playlist'
+
+    __playlist_id = Column('playlist_id', String, nullable=False, primary_key=True)
+    __user_id = Column('user_id', String, ForeignKey('User.user_id'), nullable=False)
+
+
+class User(Base):
+    __tablename__ = 'User'
+
+    __user_id = Column('user_id', String, nullable=False, primary_key=True)
+    __username = Column('username', String, nullable=True)
+    __email = Column('email', String, nullable=True)
+    __password = Column('password', String, nullable=True)
+    __apple_id = Column('apple_id', String, nullable=True)
+    __github_id = Column('github_id', String, nullable=True)
+    __google_id = Column('google_id', String, nullable=True)
+    __spotify_id = Column('spotify_id', String, nullable=True)
+
+    playlists = relationship('PlaylistMap')
+
+    def __init__(self):
+        self.__user_id = str(uuid4())
+
+    def __setitem__(self, key, value):
+        match key:
+            case 'apple_id':
+                self.__apple_id = value
+            case 'github_id':
+                self.__github_id = value
+            case 'google_id':
+                self.__google_id = value
+            case 'spotify_id':
+                self.__spotify_id = value
+            case _:
+                raise KeyError
+        session.commit()
+
+    @property
+    def id(self):
+        return self.__user_id
+
+    @classmethod
+    def get_by(cls, method: str, _id: str):
+        if method not in {'user_id', 'apple_id', 'github_id', 'google_id', 'spotify_id'}:
+            raise KeyError
+        user = session.query(cls).filter(getattr(cls, f'_{cls.__name__}__{method}') == _id).first()
+        if user is None:
+            user = User()
+            session.add(user)
+            user[method] = _id
+        return user
+
+
+# TODO: Temporary value and file name
+db = create_engine('sqlite:///data.db', echo=False, connect_args={'timeout': 30})
+Base.metadata.create_all(bind=db)
+session = sessionmaker(bind=db)()
